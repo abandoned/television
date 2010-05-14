c0a98e90b971e17ed682d637d98a046e8f14b1a8
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-05-14 22:57:00 +0100

    Will figure out octopi later

--- television.rb
+++ television.rb
@@ -2,13 +2,16 @@ require "rubygems"
 require "bundler"
 Bundler.setup
 
-require 'octopi'
-require 'sinatra/base'
-require 'erb'
+require "octopi"
+require "sinatra/base"
+require "erb"
 
 class Television < Sinatra::Base
-  get '/' do
+  include Octopi
+  
+  get "/" do
     authenticated_with :config => "github.yml" do
+      #Repository.find_all(:user => config["github"]["login"])
     end
     
     erb :index
