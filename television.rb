feaee9521e64594cb057d9506ef4993030bdfd9d
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-05-15 12:47:42 +0100

    Patch Octopussy to handle private repos

--- television.rb
+++ television.rb
@@ -6,13 +6,24 @@ require "octopussy"
 require "sinatra/base"
 require "erb"
 
-class Television < Sinatra::Base
-  
-  get "/" do
-    authenticated_with :config => "github.yml" do
-      #Repository.find_all(:user => config["github"]["login"])
+# Patch Octopussy to handle private repos
+module Octopussy
+  class Client
+    def list_repos(username)
+      response = self.class.get("/repos/show/#{username}", :query => auth_params)
+      Hashie::Mash.new(response).repositories
     end
-    
+  
+    def list_commits(repo, branch="master")
+      repo = Repo.new(repo)
+      response = self.class.get("/commits/list/#{repo.username}/#{repo.name}/#{branch}", :query => auth_params)
+      Hashie::Mash.new(response).commits
+    end
+  end
+end
+
+class Television < Sinatra::Base
+  get "/" do
     erb :index
   end
 end
